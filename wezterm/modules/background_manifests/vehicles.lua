52bebe95ac17f1762e306e285a4ce26cfcb4e5d9
ajason13 <a.jason13@gmail.com>
2026-08-08 08:09:16 -0700

    feat(backgrounds): update GR Corolla to GRMN

--- wezterm/modules/background_manifests/vehicles.lua
+++ wezterm/modules/background_manifests/vehicles.lua
@@ -1,6 +1,6 @@
 return {
   '100-vehicles/110-ae86-rainy-mountain-pass.png',
-  '100-vehicles/150-gr-corolla-foggy-mountain-pass.png',
+  '100-vehicles/150-grmn-corolla-foggy-mountain-pass.png',
   '100-vehicles/160-wrx-rainy-mountain-pass.png',
   '100-vehicles/170-mini-cooper-s-foggy-mountain-pass.png',
   '100-vehicles/180-1955-chevy-gasser-night-drag.png',
